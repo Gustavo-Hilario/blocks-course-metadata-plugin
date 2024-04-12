b7cea6afe0578f5fbda42101e7f742e6249d878f
Gustavo_H <gustavo.hilario@automattic.com>
2024-04-12 08:25:37 -0500

    104. Customizing the Existing Editor Ul

--- src/plugins/sidebar.js
+++ src/plugins/sidebar.js
@@ -1,16 +1,77 @@
 //eslint-disable-next-line
 import { registerPlugin } from '@wordpress/plugins';
-import { PluginSidebar } from '@wordpress/edit-post';
+import {
+	PluginSidebar,
+	PluginDocumentSettingPanel,
+	PluginPostStatusInfo,
+	PluginPrePublishPanel,
+	PluginPostPublishPanel,
+	PluginMoreMenuItem,
+	PluginBlockSettingsMenuItem,
+} from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
 
 registerPlugin('metadata-plugin', {
 	render: () => (
-		<PluginSidebar
-			name="metadata-field-sidebar"
-			icon="admin-settings"
-			title={__('Post Option', 'metadata-plugin')}
-		>
-			<p>Metadata settings</p>
-		</PluginSidebar>
+		<>
+			<PluginDocumentSettingPanel
+				name="metadata-field-panel"
+				title={__('Post Option', 'metadata-plugin')}
+				icon="admin-collapse"
+			>
+				<p>Metadata settings</p>
+			</PluginDocumentSettingPanel>
+			<PluginPostStatusInfo>
+				<p
+					style={{
+						fontSize: '16px',
+						fontWeight: 'bold',
+						color: 'red',
+					}}
+				>
+					Metadata settings
+				</p>
+			</PluginPostStatusInfo>
+
+			<PluginPrePublishPanel
+				title="Pre Publish Panel"
+				icon="admin-settings"
+			>
+				<p>Metadata Pre Publish Panel</p>
+			</PluginPrePublishPanel>
+
+			<PluginPostPublishPanel
+				title="Post Publish Panel"
+				icon="admin-settings"
+			>
+				<p>Metadata Post Publish Panel</p>
+			</PluginPostPublishPanel>
+
+			<PluginMoreMenuItem
+				icon="admin-customizer"
+				onClick={() => {
+					alert('Metadata settings');
+				}}
+			>
+				{__('Metadata settings', 'metadata-plugin')}
+			</PluginMoreMenuItem>
+
+			<PluginBlockSettingsMenuItem
+				label="Metadata Plugin Test"
+				allowedBlocks={['core/paragraph']}
+				icon="admin-customizer"
+				onClick={() => {
+					alert('Metadata settings');
+				}}
+			></PluginBlockSettingsMenuItem>
+
+			<PluginSidebar
+				name="metadata-field-sidebar"
+				icon="admin-settings"
+				title={__('Post Option', 'metadata-plugin')}
+			>
+				<p>Metadata settings</p>
+			</PluginSidebar>
+		</>
 	),
 });
